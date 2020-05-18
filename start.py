38f9333e5e43b75131a0cb5d47b861b235edd6f4
Vignesh <vignesh.shankar@solitontech.com>
2020-05-19 02:41:38 +0530

    Create Machine class to store related information
    Signed-off-by: Vignesh <vignesh.shankar@solitontech.com>

--- start.py
+++ start.py
@@ -1,33 +1,45 @@
+import json
+
+class Machine():
+    def __init__(self, name, capacity, cost_per_hr=None):
+        self.name = name
+        self.capacity = capacity
+        self.cost_per_hr = cost_per_hr
+        self.cost_per_unit = None
+
+        if self.cost_per_hr != None:
+            self.cost_per_unit = self.cost_per_hr / self.capacity
+
 
 def allocate_machines(machines, required_capacity):
     filtered_machines = []
-    for machine in machines:
-        machine_name, capacity, cost_per_hr = machine
-        # Filter out if cost_per_hr is not available
-        if cost_per_hr == None:
-            continue
-        cost_per_unit = cost_per_hr / capacity
-        filtered_machines.append(
-            (machine_name, capacity, cost_per_hr, cost_per_unit)
-        )
+    for spec_tuple in machines:
+        _machine = Machine(*spec_tuple)
 
-    # Sort filtered machines based on cost_per_unit
-    machine_priorities = sorted(filtered_machines, key=lambda x: x[3])
+        # Only append machines that have cost_per_hr specified
+        if _machine.cost_per_hr:
+            filtered_machines.append(_machine)
+
+    # Prioritise machines with least cost_per_unit
+    machine_priorities = sorted(
+        filtered_machines,
+        key=lambda machine: machine.cost_per_unit
+    )
 
     allocated_machines = []
     for machine in machine_priorities:
-        machine_name, capacity, cost_per_hr, _ = machine
-        reqd_no_of_machines = required_capacity // capacity
-        if reqd_no_of_machines == 0:
+        reqd_no_of_machines = required_capacity // machine.capacity
+        if reqd_no_of_machines <= 0:
             continue
-        required_capacity %= capacity
+        # Subtract maximum counts of machine capacity possible
+        required_capacity %= machine.capacity
         allocated_machines.append(
-            (machine_name, reqd_no_of_machines, cost_per_hr)
+            (machine.name, reqd_no_of_machines, machine.cost_per_hr)
         )
     return allocated_machines
 
-if __name__ == '__main__':
 
+if __name__ == '__main__':
     region_to_machines_map = {
         # (machine_name, capacity, cost_per_hr)
         'New York': [
@@ -39,20 +51,20 @@ if __name__ == '__main__':
             ('10XLarge', 320, 2820)
         ],
         'India': [
-            ('Large', 10, 120),
-            ('XLarge', 20, 230),
-            ('2XLarge', 40, 450),
-            ('4XLarge', 80, 774),
-            ('8XLarge', 160, 1400),
-            ('10XLarge', 320, 2820)
+            ('Large', 10, 140),
+            # ('XLarge', 20, None),
+            ('2XLarge', 40, 413),
+            ('4XLarge', 80, 890),
+            ('8XLarge', 160, 1300),
+            ('10XLarge', 320, 2970)
         ],
         'China': [
-            ('Large', 10, 120),
-            ('XLarge', 20, 230),
-            ('2XLarge', 40, 450),
-            ('4XLarge', 80, 774),
-            ('8XLarge', 160, 1400),
-            ('10XLarge', 320, 2820)
+            ('Large', 10, 110),
+            ('XLarge', 20, 200),
+            # ('2XLarge', 40, None),
+            ('4XLarge', 80, 670),
+            ('8XLarge', 160, 1180),
+            # ('10XLarge', 320, None)
         ]
     }
 
@@ -60,14 +72,24 @@ if __name__ == '__main__':
     print('Capacity:    {}'.format(required_capacity))
     print('Hours:       {}'.format(total_hrs))
 
-    result = []
+    results = {
+        'Output': []
+    }
 
     for region, all_machines in region_to_machines_map.items():
-        allocated_machines = allocate_machines(all_machines, required_capacity)
-        result.append({
-            'region': region,
-            'machines': allocated_machines,
-            'total_cost': '$'
-        })
+        result = {}
+        result['region'] = region
+        result['machines'] = []
 
-    print(result)
+        total_cost = 0
+        for (
+            machine_name, reqd_no_of_machines, run_cost_per_hr
+        ) in allocate_machines(all_machines, required_capacity):
+            result['machines'].append((machine_name, reqd_no_of_machines))
+            total_cost += (reqd_no_of_machines * run_cost_per_hr * total_hrs)
+
+        result['total_cost'] = '${}'.format(total_cost)
+        results['Output'].append(result)
+
+    # print(results)
+    print(json.dumps(results, indent=4))
