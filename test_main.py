9e610b34b92b5dbc6624491914c6a3c16658a164
Vignesh <vignesh.shankar@solitontech.com>
2020-05-20 03:51:48 +0530

    Add basic unit tests for functions
    Signed-off-by: Vignesh <vignesh.shankar@solitontech.com>

--- test_main.py
+++ test_main.py
@@ -1,4 +1,25 @@
-from main import allocate_machines
+from main import parse_input, allocate_machines, allocate_machines_regionally, REGION_TO_MACHINES_MAP
+import pytest
+
+@pytest.mark.parametrize("input_string,expected_output", [
+    ('Capacity of 1150 units for 1 Hour', (1150, 1)),
+    ('230 units for 5 Hours', (230, 5)),
+    ('100 units for 24 Hours', (100, 24)),
+    ('1100 units for 12 Hours', (1100, 12))
+])
+def test_parse_input(input_string, expected_output):
+    assert parse_input(input_string) == expected_output
+
+@pytest.mark.parametrize("country,allocated_machines", [
+    ('New York', [('8XLarge', 7, 1400), ('XLarge', 1, 230), ('Large', 1, 120)]),
+    ('India', [('8XLarge', 7, 1300), ('Large', 3, 140)]),
+    ('China', [('8XLarge', 7, 1180), ('XLarge', 1, 200), ('Large', 1, 110)])
+])
+def test_allocate_machines_regionally(country, allocated_machines):
+    assert allocate_machines_regionally(
+        REGION_TO_MACHINES_MAP[country], 1150
+    ) == allocated_machines
+
 
 def test_allocate_machines():
     assert allocate_machines(1150, 1) == {
